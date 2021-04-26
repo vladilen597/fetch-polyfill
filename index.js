157dfd6c0c7b785b552a64d166633087db27e9df
Uladzilen Isayenia <Uladzilen.Isayenia@itechart-group.com>
2021-04-26 13:33:48 +0300

    if updates

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
-function customFetch(url, ...{ ...arg }) {
+async function customFetch(url, ...arg) {
   let xhr = new XMLHttpRequest();
   let xhrCallback;
   let errorCallback;
-  let toSend = null;
+  let toSend;
 
   let method = "GET";
-  if (arg[0].method == "GET" || arg[0].method == "POST") {
-    method = arg[0].method;
+  if (arg.length == 0) {
     xhr.open(method, url, true);
     xhr.responseType = "json";
-  } else if (arg[0].method == "PUT" || arg[0].method == "PATCH") {
-    method = arg[0].method;
-    let headersKey = Object.keys(arg[0].headers);
-    let headersValue = Object.values(arg[0].headers);
-    xhr.open(method, url, true);
-    xhr.setRequestHeader(headersKey[0], headersValue[0]);
-    toSend = arg[0].body;
-    xhr.responseType = "json";
+  } else {
+    if (arg[0].method == "GET" || arg[0].method == "POST") {
+      method = arg[0].method;
+      xhr.open(method, url, true);
+      xhr.responseType = "json";
+    }
+    if (arg[0].method == "PUT" || arg[0].method == "PATCH") {
+      method = arg[0].method;
+      let headersKey = Object.keys(arg[0].headers);
+      let headersValue = Object.values(arg[0].headers);
+      xhr.open(method, url, true);
+      xhr.setRequestHeader(headersKey[0], headersValue[0]);
+      toSend = JSON.stringify(arg[0].body);
+      xhr.responseType = "json";
+    }
   }
 
   xhr.send(toSend);
@@ -42,13 +48,16 @@ function customFetch(url, ...{ ...arg }) {
 
 customFetch("https://jsonplaceholder.typicode.com/posts/1", {
   method: "PUT",
-  body: JSON.stringify({
-    id: 3,
-    title: "foo",
+  body: {
+    id: 1,
     body: "bar",
     userId: 1,
-  }),
+  },
   headers: { "Content-Type": "application/json" },
 }).then((a) => {
+  console.log(a);
+});
+
+customFetch("https://jsonplaceholder.typicode.com/posts/1").then((a) => {
   console.log(a);
 });
